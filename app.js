34ffb9659180a21d8867361bdf9efa56bdd104f0
Altusha4 <240782@astanait.edu.kz>
2025-12-10 22:19:56 +0600

    Add POST /calculate route to process BMI and return result

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/calculate", function(req, res) {
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+    const bmi = weight / (height * height);
+    res.send("Your BMI is: " + bmi.toFixed(2));
 });
 
 app.listen(3000, function() {
